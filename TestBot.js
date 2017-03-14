cefe2bab2759cb06e9179e1abfc886e06c6728ab
cvillicana <villicana.ca@gmail.com>
2017-03-14 00:22:19 -0600

    changed module

--- TestBot.js
+++ TestBot.js
@@ -1,36 +1,14 @@
-const TeleBot = require('telebot');
-const bot = new TeleBot('343982108:AAEwO6G-rOgFBzoLduwkGXBj4RGzvC9LHxw');
-var lastMessage;
-var photoUrl = 'https://telegram.org/img/tl_card_destruct.gif'
+const Telegraf = require('telegraf')
 
-bot.on('/start', msg => {
+const app = new Telegraf('344380587:AAGY7klRRuGSCGnBvonuqI1XljdpzGJ0K4s')
 
-  // Send image with caption
-  return bot.sendPhoto(
-    msg.from.id, photoUrl, { caption: 'This is a default caption.' }
-  ).then(re => {
-    // Get message id and chat
-    lastMessage = [msg.from.id, re.result.message_id];
-    bot.sendMessage(msg.from.id, 'Now set a new caption using /edit <caption>');
-  });
+app.command('start', (ctx) => {
+  console.log('start', ctx.from)
+  ctx.reply('Welcome!')
+})
 
-});
+app.hears('hi', (ctx) => ctx.reply('Hey there!'))
 
-bot.on('/edit', msg => {
+app.on('sticker', (ctx) => ctx.reply('👍'))
 
-  if (!lastMessage)
-    return bot.sendMessage(msg.from.id, 'Type /start and then /edit <caption>');
-
-  let [chatId, messageId] = lastMessage;
-  let caption = msg.text.replace('/edit ', '');
-
-  if (caption == '/edit') caption = 'No caption.';
-
-  // Change caption
-  return bot.editCaption({ chatId, messageId }, caption).then(x => {
-    bot.sendMessage(msg.from.id, `Caption changed to: ${ caption }`);
-  });
-
-});
-
-bot.connect();
+app.startPolling()
