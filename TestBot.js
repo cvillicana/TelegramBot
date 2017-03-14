9699861a2ae4c1584924c85f473269edebbde885
cvillicana <villicana.ca@gmail.com>
2017-03-13 23:29:58 -0600

    more fixes

--- TestBot.js
+++ TestBot.js
@@ -1,11 +1,36 @@
 const TeleBot = require('telebot');
 const bot = new TeleBot('343982108:AAEwO6G-rOgFBzoLduwkGXBj4RGzvC9LHxw');
-// Setup polling way
-bot.on('text', msg => {
-  let fromId = msg.from.id;
-  let firstName = msg.from.first_name;
-  let reply = msg.message_id;
-  return bot.sendMessage(fromId, `Welcome, ${ firstName }!`, { reply });
+var lastMessage;
+var photoUrl = 'https://telegram.org/img/tl_card_destruct.gif'
+
+bot.on('/start', msg => {
+
+  // Send image with caption
+  return bot.sendPhoto(
+    msg.from.id, photoUrl, { caption: 'This is a default caption.' }
+  ).then(re => {
+    // Get message id and chat
+    lastMessage = [msg.from.id, re.result.message_id];
+    bot.sendMessage(msg.from.id, 'Now set a new caption using /edit <caption>');
+  });
+
+});
+
+bot.on('/edit', msg => {
+
+  if (!lastMessage)
+    return bot.sendMessage(msg.from.id, 'Type /start and then /edit <caption>');
+
+  let [chatId, messageId] = lastMessage;
+  let caption = msg.text.replace('/edit ', '');
+
+  if (caption == '/edit') caption = 'No caption.';
+
+  // Change caption
+  return bot.editCaption({ chatId, messageId }, caption).then(x => {
+    bot.sendMessage(msg.from.id, `Caption changed to: ${ caption }`);
+  });
+
 });
 
 bot.connect();
